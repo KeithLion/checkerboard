9d19a702b23f749091c7f7619b84699a189654b1
KeithLion <nortonkiara6@gmail.com>
2021-12-27 13:12:30 -0800

    in all files added the initial code to add custom colors to the board

--- checkerboard.py
+++ checkerboard.py
@@ -2,17 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return render_template('board.html', row = 8,col=8)
+    return render_template('board.html', row=8, col=8)
+
 
 @app.route('/<int:num>')
 def rows(num):
-    return render_template('board.html', row=num,col=num)
+    return render_template('board.html', row=num, col=num)
+
 
 @app.route('/<int:x>/<int:y>')
-def row_col(x,y):
-    return render_template('board.html', row = x,col= y)
+def row_col(x, y):
+    return render_template('board.html', row=x, col=y)
+
+
+@app.route('/<int:x>/<int:y>/<string:color1>/<string:color2>')
+def board_colors(x, y, color1, color2):
+    return render_template('board.html', row=x, col=y, color2=color2, color1=color1)
 
 
 if __name__ == "__main__":
